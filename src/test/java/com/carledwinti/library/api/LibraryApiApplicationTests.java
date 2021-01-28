84155f639d75ca31805b1571af7ce3c30431f527
Carl Edwin Antonio Nascimento <carlinstr@gmail.com>
2021-01-28 15:23:18 -0300

    Initial commit

---
+++ src/test/java/com/carledwinti/library/api/LibraryApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.carledwinti.library.api;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LibraryApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
