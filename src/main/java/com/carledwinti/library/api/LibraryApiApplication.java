9cfbbdf34d442021eee5e158a05a68f7a296ad6c
Carl Edwin Antonio Nascimento <carlinstr@gmail.com>
2021-01-28 18:07:48 -0300

    Configuração do ModelMapper para Converter automático entre Entity e Dto.

--- src/main/java/com/carledwinti/library/api/LibraryApiApplication.java
+++ src/main/java/com/carledwinti/library/api/LibraryApiApplication.java
@@ -1,10 +1,17 @@
 package com.carledwinti.library.api;
 
+import org.modelmapper.ModelMapper;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
 
 @SpringBootApplication
 public class LibraryApiApplication {
+
+	@Bean //criando uma instancia Singleton para disponibilizar no context para outras classes via Injection
+	public ModelMapper modelMapper(){
+	return new ModelMapper();
+	}
 
 	public static void main(String[] args) {
 		SpringApplication.run(LibraryApiApplication.class, args);
--- src/main/java/com/carledwinti/library/api/resource/BoockController.java
+++ src/main/java/com/carledwinti/library/api/resource/BoockController.java
@@ -3,6 +3,7 @@ package com.carledwinti.library.api.resource;
 import com.carledwinti.library.api.dto.BookDTO;
 import com.carledwinti.library.api.model.Book;
 import com.carledwinti.library.api.service.BookService;
+import org.modelmapper.ModelMapper;
 import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.*;
 
@@ -11,28 +12,37 @@ import org.springframework.web.bind.annotation.*;
 public class BoockController {
 
     private BookService bookService;
+    private ModelMapper modelMapper; //existe a necessida de adicioná-lo ao context declarando
+    // um @Bean na class de inicialização **LibraryApiApplication para que ela possa ser injetada aqui via constructor
 
     //criado o constructor para injetar o bookService no controller ao inves de utilizar o @Autowired
-    public BoockController(BookService bookService){
+    public BoockController(BookService bookService, ModelMapper modelMapper){
         this.bookService = bookService;
+        this.modelMapper = modelMapper;
     }
 
     @PostMapping
     @ResponseStatus(HttpStatus.CREATED)
     public BookDTO create(@RequestBody BookDTO bookDTO){
-        Book bookEntity = Book.builder()
-                .author(bookDTO.getAuthor())
-                .title(bookDTO.getTitle())
-                .isbn(bookDTO.getIsbn())
-                .build();
+
+//        Book bookEntity = Book.builder()
+//                .author(bookDTO.getAuthor())
+//                .title(bookDTO.getTitle())
+//                .isbn(bookDTO.getIsbn())
+//                .build();
+
+        //converter via modelMapper
+        Book bookEntity = modelMapper.map(bookDTO, Book.class);
 
         bookEntity = bookService.save(bookEntity);
 
-        return BookDTO.builder()
-                .id(bookEntity.getId())
-                .author(bookEntity.getAuthor())
-                .title(bookEntity.getTitle())
-                .isbn(bookEntity.getIsbn())
-                .build();
+//        return BookDTO.builder()
+//                .id(bookEntity.getId())
+//                .author(bookEntity.getAuthor())
+//                .title(bookEntity.getTitle())
+//                .isbn(bookEntity.getIsbn())
+//                .build();
+
+        return modelMapper.map(bookEntity, BookDTO.class);
     }
 }
